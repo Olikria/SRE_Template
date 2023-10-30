18cdfe185929c82466689d0caa6dfbd1b34d64c1
Olikria <149429482+Olikria@users.noreply.github.com>
2023-10-30 16:49:35 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -12,37 +12,31 @@ function updateImage() {
 function downloadImage() {
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d');
-    const image = document.getElementById('templateImage');
+    const image = new Image();
     const titleOverlay = document.getElementById('jobTitleOverlay');
 
-    canvas.width = image.width;
-    canvas.height = image.height;
+    image.crossOrigin = 'Anonymous';  // This can help bypass some security restrictions
+    image.src = 'We_Are_Hiring_Template_for_HR.png'; 
 
-    // Ensure the image is fully loaded before drawing on the canvas
     image.onload = function() {
-        // Draw the image on the canvas
+        canvas.width = image.width;
+        canvas.height = image.height;
+
         ctx.drawImage(image, 0, 0);
 
-        // Draw the text on the canvas
         ctx.font = '35px Montserrat';
         ctx.fillStyle = 'white';
         ctx.textAlign = 'center';
         ctx.textBaseline = 'middle';
         ctx.fillText(titleOverlay.textContent, canvas.width / 2, 642);
 
-        // Create a link and trigger the download
         const link = document.createElement('a');
         link.download = 'job_posting_image.png';
         link.href = canvas.toDataURL();
-        document.body.appendChild(link); // Temporarily add to the document
+        document.body.appendChild(link);
         link.click();
-        document.body.removeChild(link); // Clean up by removing the temporary link
+        document.body.removeChild(link);
     };
-
-    // This is to handle cases where the image might already be loaded
-    if (image.complete) {
-        image.onload();
-    }
 }
 
 document.getElementById('downloadBtn').addEventListener('click', downloadImage);
